167553c25bac9df15d73b0014730a8244d85fdf1
kusutoqlu <74954589+kusutoqlu@users.noreply.github.com>
2023-03-08 15:00:39 +0300

    Add files via upload

---
+++ Uygulama1/Uygulama1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama1
+{
+    public class Ev
+    {
+
+        public string sokakAdi;
+
+        public void sokak_ismi_ne(string a)
+        {
+            sokakAdi = a;
+        }
+
+        public int kapiNo;
+
+        public void kapi_numarasi_ne(int b)
+        {
+            kapiNo = b;
+
+        }
+        public void ekrana_yaz()
+        {
+            Console.WriteLine("Adres: " + sokakAdi);
+            Console.WriteLine("NO: " + kapiNo);
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Ev ilkev = new Ev();
+                Ev ikinciev = new Ev();
+                Ev ucuncuev = new Ev();
+
+                ilkev.sokak_ismi_ne("Menekşe Sokak");
+                ilkev.kapi_numarasi_ne(123);
+                ilkev.ekrana_yaz();
+
+                ikinciev.sokak_ismi_ne("Yasemin Sokak");
+                ikinciev.kapi_numarasi_ne(456);
+                ikinciev.ekrana_yaz();
+
+                ucuncuev.sokak_ismi_ne("Menekşe Sokak");
+                ucuncuev.kapi_numarasi_ne(123);
+                ucuncuev.ekrana_yaz();
+
+                Console.ReadKey();
+
+
+
+
+            }
+    }
+}
