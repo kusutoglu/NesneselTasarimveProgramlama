6935f5745afb5fe6382e558fbce467ed251652d8
kusutoqlu <74954589+kusutoqlu@users.noreply.github.com>
2023-03-22 22:43:18 +0300

    Add files via upload

---
+++ uygulama3/uygulama3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("10 haneli sayı giriniz: ");
+            
+            int s = Convert.ToInt32(Console.ReadLine());
+            
+            int sayac = 0;
+
+
+            if (sayac>11)
+            {
+                Console.WriteLine("Fazla sayı girdiniz");
+                Console.WriteLine("10 haneli sayı giriniz: ");
+            }
+
+
+            else
+            {
+        
+
+                while (s > 0)
+                {
+                    s /= 10;
+                    sayac++;
+                }
+
+                Console.WriteLine("Girilen sayıda  " + sayac + " hane vardır.");
+            }
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
