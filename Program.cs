3fae247947c73e68f32dee8f0f2ce58c1e601220
kusutoqlu <74954589+kusutoqlu@users.noreply.github.com>
2023-03-22 23:38:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,25 +10,12 @@ namespace uygulama3
     {
         static void Main(string[] args)
         {
-
+            a1:
             Console.WriteLine("10 haneli sayı giriniz: ");
-            
-            int s = Convert.ToInt32(Console.ReadLine());
-            
+            UInt64 s = Convert.ToUInt64(Console.ReadLine());
             int sayac = 0;
-
-
-            if (sayac>11)
+            if (sayac < 11)
             {
-                Console.WriteLine("Fazla sayı girdiniz");
-                Console.WriteLine("10 haneli sayı giriniz: ");
-            }
-
-
-            else
-            {
-        
-
                 while (s > 0)
                 {
                     s /= 10;
@@ -37,10 +24,13 @@ namespace uygulama3
 
                 Console.WriteLine("Girilen sayıda  " + sayac + " hane vardır.");
             }
-
-            Console.ReadKey();
-
+            if (sayac > 10)
+            {
+                Console.WriteLine("Fazla sayı girdiniz");
+                Console.WriteLine("10 haneli sayı giriniz: ");
+                goto a1;
+            }
         }
-        
+
     }
 }
